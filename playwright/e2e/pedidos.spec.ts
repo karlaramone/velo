fe91f42003fe0a5e6b202435e5c29b24b374279c
Karla <karlaramone@gmail.com>
2026-06-04 12:48:44 -0300

    AAA - Arrange, Act, Assert

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+/// AAA - Arrange, Act, Assert
+
+test('deve consultar um pedido aprovado', async ({ page }) => {
+  // Arrange
+  await page.goto('http://localhost:5173/');
+
+  //checkpoint
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+
+  // Act
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+
+  //checkpoint
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+
+  // Act
+  await page.getByTestId('search-order-id').fill('VLO-HBZVBE');
+
+  // Act
+  await page.getByTestId('search-order-button').click();
+
+  // Assert
+  await expect(page.getByTestId('order-result-status')).toBeVisible();
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-HBZVBE');
+
+  // Assert
+  await expect(page.getByTestId('order-result-status')).toBeVisible();
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+});
